7909ae30e5acaabfba81de48b2cb420e1fe95f2a
slowpoke <mail+git@slowpoke.io>
2013-02-25 22:54:16 +0000

    Basic ln implementation (no flags).

---
+++ cmd/ln/ln.go
@@ -0,0 +1,84 @@
+// gotils - the Go coreutils
+//
+// Author: slowpoke <mail+git@slowpoke.io>
+//
+// This program is free software under the non-terms
+// of the Anti-License. Do whatever the fuck you want.
+//
+// Github: https://www.github.com/proxypoke/gotils
+//
+// Format options for vim. Please adhere to them.
+// vim: set et ts=4 sw=4 tw=80:
+
+// ln - make links between files
+package main
+
+import (
+	"flag"
+	"gotils/shared/msg"
+	"os"
+	"path"
+)
+
+// The first form of ln. Links the target file to the named file.
+func first_form(target, name string) (err error) {
+	err = os.Link(target, name)
+	if err != nil {
+		msg.Errln(err)
+	}
+	return
+}
+
+// The second form of ln. Links the target file to the current directory.
+func second_form(target string) (err error) {
+	name := path.Join(".", path.Base(target))
+	err = first_form(target, name)
+	return
+}
+
+// The third form of ln. Links a list of targets to a directory.
+func third_form(dir string, targets ...string) (err error) {
+	for _, target := range targets {
+		name := path.Join(dir, path.Base(target))
+		if e := first_form(target, name); e != nil {
+			err = e
+		}
+	}
+	return
+}
+
+func main() {
+	var (
+		paths []string
+		err   error
+	)
+	flag.Parse()
+
+	paths = flag.Args()
+
+	l := len(paths)
+	switch {
+	// No arguments.
+	case l < 1:
+		msg.Errln("Not enough arguments.")
+		os.Exit(1)
+	// First form.
+	case l == 2:
+		target := paths[0]
+		name := paths[1]
+		err = first_form(target, name)
+	// Second form.
+	case l == 1:
+		target := paths[0]
+		err = second_form(target)
+	// Third form.
+	case l > 2:
+		targets := paths[:len(paths)-1]
+		dir := paths[len(paths)-1]
+		err = third_form(dir, targets...)
+	}
+
+	if err != nil {
+		os.Exit(1)
+	}
+}
