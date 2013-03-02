proxypoke/gotils

commit 3f871ec86ad8d2505f2bbc12cca1f1661892962a

Some work on fileutils.go
Added an IsDir function, simplifying Move() and cp a bit.

cmd/cp/cp.go
@@ -31,18 +31,9 @@ func main() {
 	}
 	src, dest := files[0], files[1]
 
-	info, err := os.Stat(dest)
-	if err != nil {
-		msg.Errf("cp: %s\n", err)
-		os.Exit(1)
-	}
-	if info.IsDir() {
-		dest = path.Join(dest, path.Base(src))
-	}
-
 	err = shared.Copy(src, dest)
 	if err != nil {
-		msg.Errln(err)
+		msg.Errf("cp: %s\n", err)
 		os.Exit(1)
 	}
 }

shared/fileutils.go
@@ -18,24 +18,44 @@ import (
 	"path"
 )
 
+// Check if a path is a directory.
+// This will return true if and only if the path is a directory. Any other
+// condition is treated as false - including nonexistence.
+func IsDir(path string) bool {
+	info, err := os.Stat(path)
+	if err == nil {
+		return info.IsDir()
+	}
+	return false
+}
+
 // Copy a file.
 // If dest is a directory, then src will be copied into that directory.
 func Copy(src, dest string) (err error) {
+	// Check if the destination is a directory.
+	// If it is, create the correct destination path by joining the destination
+	// path with the basename of the source.
+	if IsDir(dest) {
+		dest = path.Join(dest, path.Base(src))
+	}
+
+	// Open the souce file.
 	src_file, err := os.Open(src)
 	if err != nil {
-        return
+		return
 	}
 	defer src_file.Close()
 
+	// Open the destination file.
 	dest_file, err := os.Create(dest)
 	if err != nil {
-        return
+		return
 	}
 	defer dest_file.Close()
 
-    // Why the hell are the arguments to Copy the wrong way around?
+	// Why the hell are the arguments to Copy the wrong way around?
 	_, err = io.Copy(dest_file, src_file)
-    return
+	return
 }
 
 // Move a file to a different location.
@@ -45,19 +65,13 @@ func Copy(src, dest string) (err error) {
 // NOTE: Move tries to avoid unnecessary IO. It first tries to just rename a
 // file, and only actually moves it when that doesn't work.
 func Move(src, dest string) error {
-	// Check if the destination is a directory.
-	// If it is, create the correct destination path by joining the destination
-	// path with the basename of the source.
-	info, err := os.Stat(dest)
-	if err != nil {
-		return err
-	}
-	if info.IsDir() {
+	// see Copy()
+	if IsDir(dest) {
 		dest = path.Join(dest, path.Base(src))
 	}
 
 	// First, try to just rename it, causing no data to be moved around.
-	err = os.Rename(src, dest)
+	err := os.Rename(src, dest)
 	if err != nil {
 		// If that doesn't work, most likely a cross-device move was deleted.
 		// That means we have to actually move the data. We copy it over, then

commit 8bd2907c7b960d76da4c485ca3b421a761a02fcd

3f871ec broke cp. Fixed.

cmd/cp/cp.go
@@ -18,7 +18,6 @@ import (
 	"gotils/shared"
 	"gotils/shared/msg"
 	"os"
-	"path"
 )
 
 func main() {
@@ -31,7 +30,7 @@ func main() {
 	}
 	src, dest := files[0], files[1]
 
-	err = shared.Copy(src, dest)
+	err := shared.Copy(src, dest)
 	if err != nil {
 		msg.Errf("cp: %s\n", err)
 		os.Exit(1)